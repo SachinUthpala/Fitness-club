0ca1a933bf7efce71399a06d8a7a3b0f73812be5
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-01-15 14:35:23 +0530

    email

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Join from './components/Join/Join';
 import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
@@ -13,6 +14,8 @@ function App() {
           <Reasons />
           <Plans/>
           <Testimonials/>
+          <Join />
+        
     </div>
   );
 }
--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Join.css'
+import emailjs from '@emailjs/browser';
 
 const Join = () => {
+    
+    //email sending
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
   return (
-    <div>Join</div>
+    <div className="join" id="join">
+        
+        <div className="left-j">
+        <hr />
+            <div>
+                <span className='stroke-text'>READY TO</span>
+                <span >LEVEL UP</span>
+            </div>
+            <div>
+                <span>YOUR BODY</span>
+                <span className='stroke-text'>WITH US</span>
+            </div>
+        </div>
+        <div className="right-j">
+            <form ref={form} className="email-container" onSubmit={sendEmail}>
+                <input type="email" name="user_email" id="" placeholder='Enter Your Mail' />
+                <button className="btn-j btn"> Join Now </button>
+            </form>
+        </div>
+    </div>
+
   )
 }
 
